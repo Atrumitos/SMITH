8a6d19af0b0ebce187cd321b427d70c7d81f873b
Atrumitos <aris.g.g@outlook.com>
2025-07-17 20:41:05 +0300

    C: It's now an agent! (course complete)

--- main.py
+++ main.py
@@ -51,27 +51,43 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
-    )
+    MAX_ITERATIONS = 20
+    iteration=0
+    success=False
+    while iteration<MAX_ITERATIONS:
+        try:
+            iteration+=1
+            response = client.models.generate_content(
+                model="gemini-2.0-flash-001",
+                contents=messages,
+                config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
+            )
+            
+            for c in response.candidates:
+                messages.append(c.content)                
 
-    if args.verbose:
-        print(f'User prompt: "{user_prompt}"')
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-    if response.function_calls:
-        from functions.call_function import call_function
-        for function_call_part in response.function_calls:
-            function_call_result=call_function(function_call_part, args.verbose)
-            if function_call_result.parts[0].function_response.response and args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-            elif not function_call_result.parts[0].function_response.response:
-                raise Exception("ERROR: No result reported.")
-    else:
-        print(response.text)
+            if response.function_calls:
+                from functions.call_function import call_function
+                for function_call_part in response.function_calls:
+                    function_call_result=call_function(function_call_part, args.verbose)
+                    if function_call_result.parts[0].function_response.response and args.verbose:
+                        print(f'User prompt: "{user_prompt}"')
+                        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+                        print(f"-> {function_call_result.parts[0].function_response.response}")
+                    elif not function_call_result.parts[0].function_response.response:
+                        raise Exception("ERROR: No result reported.")
+                    messages.append(types.Content(role="tool", parts=function_call_result.parts))
+            else:
+                print(response.text)
+                success=True
+                break
+        except Exception as e:
+            print(f"Error during iteration {iteration}: {e}")
+            break
+    if not success:
+        print(f"{iteration} iterations out of a maximum of {MAX_ITERATIONS} were performed with no answer.")
+        return None
 
 if __name__ == "__main__":
     main()
